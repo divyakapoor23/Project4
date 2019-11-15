ad5ce2eeb434d1b493c1b76d3a58654685318b6c
divyakapoor23 <divya.kapoor878@gmail.com>
2019-11-15 11:52:50 -0800

    Initial Commit

---
+++ Project4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project4
+//
+//  Created by Divya Kapoor on 11/15/19.
+//  Copyright © 2019 Divya Kapoor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
